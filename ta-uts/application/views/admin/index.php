a08a400b09e94ec728efa2727b7e9fd62270e390
AzhahraRisky <azhahra.risky1@gmail.com>
2022-04-18 21:07:25 +0700

    Tugas UTS

---
+++ ta-uts/application/views/admin/index.php
@@ -0,0 +1,16 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <center><br><br><br><br><br><br><br><br>
+        <img src="assets/images/sushi_logo.png" width="200px height=" 200px" /> <br>
+        <font Size="6" face="Helvetica">Sistem Informasi</font> <br>
+        <font Size="6">Matsuri Castle Restaurant</font>
+    </center>
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
